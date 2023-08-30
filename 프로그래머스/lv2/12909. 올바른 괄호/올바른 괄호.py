cf7e9d91bea8170e6851773e5c4c71de9b492b06
DongKuk Lee <116425551+Greeddk@users.noreply.github.com>
2023-08-30 20:49:43 +0900

    [level 2] Title: 올바른 괄호, Time: 9.11 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,21 +1,11 @@
 def solution(s):
-    answer = True
-    stack = []
     arr = list(s)
     cnt = 0
     for i in arr:
-        if cnt < 0:
-            break
         if i == '(':
             cnt += 1
         elif i == ')':
             cnt -=  1
-            
-    
-    
-    if arr[0] == '(' and arr[-1] == ')' and cnt == 0:
-        answer = True
-    else:
-        answer = False
-        
-    return answer
+        if cnt < 0:
+            return False
+    return cnt == 0
