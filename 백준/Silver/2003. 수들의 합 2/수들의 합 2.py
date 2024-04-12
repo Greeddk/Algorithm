46650171c38e1f192ad00f653d85d28180816d1c
DongGuk Lee <116425551+Greeddk@users.noreply.github.com>
2024-04-12 16:44:07 +0900

    [Silver IV] Title: 수들의 합 2, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -5,23 +5,25 @@ n,m = map(int, input().split())
 arr = list(map(int,input().split()))
 answer = 0
 cnt = 0
-r = 0
-l = 0
+right = 0
+left = 0
 
 while True:
 
     if cnt < m:
-        if r < n: #r포인터가 전체 배열 개수보다 작으면
-            cnt += arr[r]
-            r += 1
-        else: #r포인터가 전체 배열 개수랑 같을 경우 끝! -> 어차피 l포인터를 데리고 와도 빼기밖에 안하니까 더하는 건 의미가 없음
+        if right < n:
+            cnt += arr[right]
+            right += 1
+        else:
             break
-    elif cnt > m:
-        cnt -= arr[l]
-        l += 1
-    if cnt == m:
+    elif cnt == m:
         answer += 1
-        cnt -= arr[l]
-        l += 1
+        cnt -= arr[left]
+        left += 1
+
+    else:
+        cnt -= arr[left]
+        left += 1
+
 
 print(answer)
