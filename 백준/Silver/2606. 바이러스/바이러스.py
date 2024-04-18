e9db334d597ca8440fa73c7ee0cbb5ceefc5030f
DongGuk Lee <116425551+Greeddk@users.noreply.github.com>
2024-04-18 19:22:59 +0900

    [Silver III] Title: 바이러스, Time: 68 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,23 @@
-v = int(input())
-m = int(input())
-cur_v = 1
+from collections import deque
 
-# 그래프 초기화
-graph = [[0]*(v+1) for i in range(v+1)]
+V = int(input())
+E = int(input())
 
-# 입력받아서 그래프에 값 넣기
-for i in range(m):
-    x, y = map(int, input().split())
-    graph[x][y] = 1
-    graph[y][x] = 1
+adj = [[] for _ in range(V + 1)]
+visited = set()
+queue = deque([1])
 
-# dfs 혹은 bfs를 통해 visited한 목록 구하기
-visited = [False] * (v+1)
-visited[1] = True
-def dfs(cur_v):
-    visited[cur_v] = True
-    for i in range(1, v+1):
-        if visited[i] == False and graph[cur_v][i] == 1:
-            dfs(i)
-    return visited.count(1) - 1
+for i in range(E):
+    s, e = map(int, input().split())
+    adj[s].append(e)
+    adj[e].append(s)
 
-print(dfs(1))
+while len(queue) != 0:
+    current = queue.popleft()
+    visited.add(current)
+
+    for i in adj[current]:
+        if i not in visited:
+            queue.append(i)
+
+print(len(visited) - 1)
